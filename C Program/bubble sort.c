1642b3b2de8c51317926017889492925fb6b4f2a
Nilanshu tiwary <nilanshums7@gmail.com>
2024-12-30 23:18:04 +0530

    Create bubble sort.c

---
+++ C Program/bubble sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void bubbleSort(int array[], int size) {
+    for (int step = 0; step < size - 1; ++step) {
+        for (int i = 0; i < size - step - 1; ++i) {
+            if (array[i] > array[i + 1]) {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int data[] = {5, 3, 8, 4, 2, 7};
+    int size = sizeof(data) / sizeof(data[0]);
+    bubbleSort(data, size);
+    printf("Sorted array: \n");
+    printArray(data, size);
+    return 0;
+}
